3c4cf46288cd0596518a355893f21194cc83c3ce
Dan Lynn <dan@danlynn.org>
2017-08-16 15:24:17 -0400

    Made fail-over independent for each attribute + comments

--- src/awsconfig_extra.js
+++ src/awsconfig_extra.js
@@ -2,19 +2,36 @@ import fs from 'fs'
 
 
 /**
- * Get aws config attributes for accessKeyId, secretAccessKey, region by
- * looking in the following places starting at the top then falling back
- * the following locations until found:
+ * Get aws config attributes for accessKeyId, secretAccessKey, and region
+ * by looking in the following places starting at the top then falling
+ * back to the following locations until found.  It will perform this
+ * search for each attribute individually:
  *
  *   1. command line options: --accessKeyId, --secretAccessKey, --region
  *   2. ENV vars: ACCESS_KEY_ID, SECRET_ACCESS_KEY, REGION
  *   3. standard files in workdir: .aws/credentials, .aws/config
  *   4. standard files in home: ~/.aws/credentials, ~/.aws/config
  *
- * @param options {{accessKeyId: string, secretAccessKey: string, region: string}} command line options object
+ * Note that "command-line-args" node module works well for generating
+ * the command-line 'options' arg.  The 'options' arg is optional as are
+ * the attributes found within it.
+ *
+ * If no values can be determined for any of the three attributes then
+ * null will be returned.  This is convenient for use with the aws sdk
+ * since most aws service client instances accept null when the code is
+ * running within aws.  Thus, the following code will work fine in both
+ * local development and in the aws cloud:
+ *
+ *   const sqs = new AWS.SQS(awsConfig(options))
+ *
+ * ...in local dev, you could have ENV vars set - then in aws cloud prod,
+ * you simply don't set any ENV vars and the AWS.SQS will derive your
+ * credentials from the account that it is running within.
+ *
+ * @param [options] {{[accessKeyId]: string, [secretAccessKey]: string, [region]: string}} command line options object
  * @returns {{accessKeyId: string, secretAccessKey: string, region: string}} object with aws config attributes
  */
-export default function awsConfig(options) {
+export default function awsConfig(options = null) {
   let credentialsFile
   try {
     credentialsFile = fs.readFileSync('.aws/credentials', "utf8")
@@ -40,17 +57,18 @@ export default function awsConfig(options) {
   }
   const regionMatch = configFile && configFile.match(/region\s*=\s*(.*)/)
 
-  const accessKeyId = options.accessKeyId || process.env.ACCESS_KEY_ID || (accessKeyIdMatch && accessKeyIdMatch[1])
-  const secretAccessKey = options.secretAccessKey || process.env.SECRET_ACCESS_KEY || (secretAccessKeyMatch && secretAccessKeyMatch[1])
-  const region = options.region || process.env.REGION || (regionMatch && regionMatch[1])
-  let awsAuth = {} // not needed if running within aws environment already
-  if (accessKeyId && secretAccessKey) {
-    awsAuth = {
-      accessKeyId: accessKeyId,
-      secretAccessKey: secretAccessKey
-    }
-  }
+  const accessKeyId = (options && options.accessKeyId) || process.env.ACCESS_KEY_ID || (accessKeyIdMatch && accessKeyIdMatch[1])
+  const secretAccessKey = (options && options.secretAccessKey) || process.env.SECRET_ACCESS_KEY || (secretAccessKeyMatch && secretAccessKeyMatch[1])
+  const region = (options && options.region) || process.env.REGION || (regionMatch && regionMatch[1])
+
+  let awsAuth = {}
+  if (accessKeyId)
+    Object.assign(awsAuth, {accessKeyId: accessKeyId})
+  if (secretAccessKey)
+    Object.assign(awsAuth, {secretAccessKey: secretAccessKey})
   if (region)
     Object.assign(awsAuth, {region: region})
+  if (Object.keys(awsAuth).length === 0)
+    awsAuth = null // not needed if running within aws environment already
   return awsAuth
 }
